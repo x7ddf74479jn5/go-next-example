ae99994bcd57ea8cb13e551e9693c745d7e997b8
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-16 21:45:33 +0900

    ✨ feat: impl api fetching

--- app/frontend/src/app/page.tsx
+++ app/frontend/src/app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState<string>("");
+  const getMessage = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setMessage(data.message);
+      } else {
+        const errorResponse = await response.json();
+        console.error("Error:", errorResponse);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getMessage();
+  }, []);
+
   return (
     <main>
-      <div className={"text-9xl"}>Hello World.</div>
+      <div className={"text-9xl"}>{message}</div>
     </main>
   );
 }
