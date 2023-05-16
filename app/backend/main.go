06ddaa85d73212783d18a2e8d36a702ed16a4847
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-16 22:37:01 +0900

    ✨ feat: impl dynamic routing

--- app/backend/main.go
+++ app/backend/main.go
@@ -5,25 +5,20 @@ import (
 	"backend/env"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	router := gin.Default()
-
 	router.Use(middleware.Cors())
-
 	env.Load()
-
 	port := os.Getenv("APP_PORT")
-
 	staticFilePath := os.Getenv("STATIC_FILE_PATH")
 
 	api := router.Group("/api")
 	{
-
 		api.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{
 				"message": "hello world",
@@ -31,30 +26,25 @@ func main() {
 		})
 	}
 
-	router.GET("/", func(c *gin.Context) {
-		url := staticFilePath + "/index.html"
-		resp, err := http.Get(url)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": "failed to fetch the file",
-				"error":   err.Error(),
-			})
-			return
-		}
-		defer resp.Body.Close()
-
-		c.DataFromReader(resp.StatusCode, resp.ContentLength, resp.Header.Get("Content-Type"), resp.Body, map[string]string{})
-	})
-
 	router.NoRoute(func(c *gin.Context) {
-		url := staticFilePath + c.Request.RequestURI
+		var url string
+		if c.Request.RequestURI == "/" {
+			url = staticFilePath + "/index.html"
+		} else {
+			url = staticFilePath + strings.TrimSuffix(c.Request.RequestURI, "/") + ".html"
+		}
+
 		resp, err := http.Get(url)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": "failed to fetch the file",
-				"error":   err.Error(),
-			})
-			return
+		if err != nil || resp.StatusCode == 404 {
+			url = staticFilePath + "/404.html"
+			resp, err = http.Get(url)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": "failed to fetch the file",
+					"error":   err.Error(),
+				})
+				return
+			}
 		}
 		defer resp.Body.Close()
 
