3aab804a7b5a0653c4de86b261f5e9ed3e0d0aca
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-16 21:56:52 +0900

    ✨ feat: impl cors

---
+++ app/backend/app/middleware/cors.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func Cors() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
--- app/backend/main.go
+++ app/backend/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/app/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 import "net/http"
 
 func main() {
 	engine := gin.Default()
+	engine.Use(middleware.Cors())
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "hello world",
@@ -13,3 +17,4 @@ func main() {
 	})
 	engine.Run(":8080")
 }
+
