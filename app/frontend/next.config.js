79273009c3649895959aad66770ab4eff08969f8
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-05-16 22:09:50 +0900

    🔧 config: enable static export

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  output: "export",
+  distDir: "../backend/views",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
